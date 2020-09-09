d34a7b4e21d0875a58d3aae341dadd5107577a2f
Alex <a.kramer103@gmail.com>
2020-09-09 19:13:12 -0400

    Load in new data

--- scripts/rscripts/load_turkish.R
+++ scripts/rscripts/load_turkish.R
@@ -10,7 +10,17 @@ loadTurkish <- function() {
   df.sumeyra <- bind_rows(list("Observed"=df.sumeyra.observed, "Optimal"=df.sumeyra.optimal, 
                             "Random"=df.sumeyra.random),  .id = 'baseline')
   
-  # sencalkapimi
+  # SumeyraCorrected
+  df.sumeyra.corrected.observed <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/manual/tr/Sumeyra/Sumeyra_observed_dependencies.csv",
+                                               header=TRUE, sep=","))
+  df.sumeyra.corrected.optimal  <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/manual/tr/Sumeyra/Sumeyra_optimal_dependencies.csv",
+                                               header=TRUE, sep=","))
+  df.sumeyra.corrected.random   <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/manual/tr/Sumeyra/Sumeyra_random_dependencies.csv",
+                                               header=TRUE, sep=","))
+  df.sumeyra.corrected <- bind_rows(list("Observed"=df.sumeyra.corrected.observed, "Optimal"=df.sumeyra.corrected.optimal, 
+                               "Random"=df.sumeyra.corrected.random),  .id = 'baseline')
+  
+  # SenCalKapimi
   df.sencalkapimi.observed <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/auto/tr/SenCalKapimi/SenCalKapimi_observed_dependencies.csv",
                                                header=TRUE, sep=","))
   df.sencalkapimi.optimal  <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/auto/tr/SenCalKapimi/SenCalKapimi_optimal_dependencies.csv",
@@ -20,9 +30,43 @@ loadTurkish <- function() {
   df.sencalkapimi <- bind_rows(list("Observed"=df.sencalkapimi.observed, "Optimal"=df.sencalkapimi.optimal, 
                                "Random"=df.sencalkapimi.random),  .id = 'baseline')
   
+  # SenCalKapimi Corrected
+  df.sencalkapimi.corrected.observed <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/manual/tr/SenCalKapimi/SenCalKapimi_observed_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.sencalkapimi.corrected.optimal  <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/manual/tr/SenCalKapimi/SenCalKapimi_optimal_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.sencalkapimi.corrected.random   <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/manual/tr/SenCalKapimi/SenCalKapimi_random_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.sencalkapimi.corrected <- bind_rows(list("Observed"=df.sencalkapimi.corrected.observed, "Optimal"=df.sencalkapimi.corrected.optimal, 
+                                    "Random"=df.sencalkapimi.corrected.random),  .id = 'baseline')
+  
+  # DilaKnt
+  df.dilakent.observed <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/auto/tr/DilaKent/DilaKent_observed_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.dilakent.optimal  <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/auto/tr/DilaKent/DilaKent_optimal_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.dilakent.random   <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/auto/tr/DilaKent/DilaKent_random_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.dilakent <- bind_rows(list("Observed"=df.dilakent.observed, "Optimal"=df.dilakent.optimal, 
+                                    "Random"=df.dilakent.random),  .id = 'baseline')
+  
+  # EnesBatur
+  df.enesbatur.observed <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/auto/tr/EnesBatur/EnesBatur_observed_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.enesbatur.optimal  <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/auto/tr/EnesBatur/EnesBatur_optimal_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.enesbatur.random   <- data.frame(read.table("~/Documents/research/git-projects/YouDePP/corpus/dependency_counts/auto/tr/EnesBatur/EnesBatur_random_dependencies.csv",
+                                                    header=TRUE, sep=","))
+  df.enesbatur <- bind_rows(list("Observed"=df.enesbatur.observed, "Optimal"=df.enesbatur.optimal, 
+                                    "Random"=df.enesbatur.random),  .id = 'baseline')
+  
   # Bind data
   df.all.tr <- bind_rows(list("Sumeyra"=df.sumeyra,
-                              "SenCalKapimi"=df.sencalkapimi), .id = 'channel')
+                              "SumeyraCorrected"=df.sumeyra.corrected,
+                              "SenCalKapimi"=df.sencalkapimi,
+                              "SenCalKapimiCorrected"=df.sencalkapimi.corrected,
+                              "DilaKent"=df.dilakent,
+                              "EnesBatur"=df.enesbatur), .id = 'channel')
   
   # Calculate squared sentence length to match Futrell et al.: better predictor than raw length
   df.all.tr$sent_len_sq   <- df.all.tr$total_length * df.all.tr$total_length
